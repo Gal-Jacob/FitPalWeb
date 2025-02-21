74d5513f5c0586857d31e5f34c6d80ded3b2172d
gal <gal.yaakov100@gmail.com>
2025-02-21 22:47:41 +0200

    finidhed basic ui

--- fitpal-fe/src/types.ts
+++ fitpal-fe/src/types.ts
@@ -6,3 +6,28 @@ export interface IPost {
   workout: string;
   details: string;
 }
+
+export interface IProfile {
+  name: string;
+  email: string;
+  photo: string;
+  height: string;
+  whight: string;
+  gender: 'male' | 'female';
+}
+
+export interface IExercise {
+    name: string;
+    sets: string | number;
+    reps: string;
+  }
+
+  export interface IGuideline {
+    name: string;
+    details: string;
+  }
+
+  export interface ISchedule {
+    day: string;
+    workout: string;
+  }
