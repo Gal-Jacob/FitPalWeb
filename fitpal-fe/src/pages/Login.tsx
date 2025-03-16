79e41e7f4aaf416d4a0fd0f7e606846996b99438
Ori Eluk <ori42410@gmail.com>
2025-03-16 19:36:59 +0200

    fix: styling in loginpage and signup page

--- fitpal-fe/src/pages/Login.tsx
+++ fitpal-fe/src/pages/Login.tsx
@@ -18,55 +18,53 @@ const Login: React.FC<AuthPagesProps> = ({ onSwitchPage }) => {
 
   return (
       <Container maxWidth="sm">
-        <Card sx={{ p: 4, mt: 8 }}>
-          <Box sx={{ mt: 8 }}>
-              <Typography variant="h2" gutterBottom>Welcome to FitPal</Typography>
-              <TextField 
-                label="Email" 
-                fullWidth 
-                margin="normal" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)}
-                error={!!email && !emailRegex.test(email)}
-                helperText={!!email && !emailRegex.test(email) ? 'Invalid email format' : ''}
-              />
-              <TextField 
-                  label="Password" 
-                  type="password" 
-                  fullWidth 
-                  margin="normal" 
-                  value={password} 
-                  onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button 
-                  variant="contained" 
-                  fullWidth 
-                  disabled={isDisabled}
-                  onClick={handleLogin}
-                  sx={{ mt: 2 }}
-              >
-                  Login
-              </Button>
-              
-              <Button 
-                  variant="text" 
-                  fullWidth 
-                  sx={{ mt: 2 }}
-                  onClick={onSwitchPage}
-              >
-                  Don't have an account? Sign Up
-              </Button>
-              <Typography sx={{mt: 4}} variant="h4" gutterBottom>OR</Typography>
-              <Button 
-                  variant="outlined" 
-                  fullWidth 
-                  sx={{ mt: 2 }}
-              >
-                  Continue with Google
-              </Button>
+        <Card sx={{ p: 4, mt: 8 }} >
+          <Typography variant="h2" gutterBottom>Welcome to FitPal</Typography>
+          <TextField sx={{ mt: 16 }}
+            label="Email" 
+            fullWidth 
+            margin="normal" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
+            error={!!email && !emailRegex.test(email)}
+            helperText={!!email && !emailRegex.test(email) ? 'Invalid email format' : ''}
+          />
+          <TextField 
+              label="Password" 
+              type="password" 
+              fullWidth 
+              margin="normal" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button 
+              variant="contained" 
+              fullWidth 
+              disabled={isDisabled}
+              onClick={handleLogin}
+              sx={{ mt: 2 }}
+          >
+              Login
+          </Button>
+          
+          <Button 
+              variant="text" 
+              fullWidth 
+              sx={{ mt: 2 }}
+              onClick={onSwitchPage}
+          >
+              Don't have an account? Sign Up
+          </Button>
+          <Typography sx={{mt: 4}} variant="h4" gutterBottom>OR</Typography>
+          <Button 
+              variant="outlined" 
+              fullWidth 
+              sx={{ mt: 2 }}
+          >
+              Continue with Google
+          </Button>
 
-          </Box>
-          </Card>
+        </Card>
       </Container>
   );
 };
--- fitpal-fe/src/pages/SignUp.tsx
+++ fitpal-fe/src/pages/SignUp.tsx
@@ -24,60 +24,63 @@ const SignUp: React.FC<AuthPagesProps> = ({ onSwitchPage }) => {
 
   return (
       <Container maxWidth="sm">
-          <Card sx={{ p: 4, mt: 8 }}>
-              <Typography variant="h4" gutterBottom>Sign Up</Typography>
-              <TextField 
-                  label="First Name" 
-                  fullWidth 
-                  margin="normal" 
-                  value={firstName} 
-                  onChange={(e) => setFirstName(e.target.value)}
-              />
-              <TextField 
-                  label="Last Name" 
-                  fullWidth 
-                  margin="normal" 
-                  value={lastName} 
-                  onChange={(e) => setLastName(e.target.value)}
-              />
-              <TextField 
-                  label="Email" 
-                  fullWidth 
-                  margin="normal" 
-                  value={email} 
-                  onChange={(e) => setEmail(e.target.value)}
-                  error={!!email && !emailRegex.test(email)}
-                  helperText={!!email && !emailRegex.test(email) ? 'Invalid email format' : ''}
-              />
-              <TextField 
-                  label="Password" 
-                  type="password" 
-                  fullWidth 
-                  margin="normal" 
-                  value={password} 
-                  onChange={(e) => setPassword(e.target.value)}
-                  error={!!password && !passwordSignUpRegex.test(password)}
-                  helperText={!!password && !passwordSignUpRegex.test(password) ? 'Password must be at least 8 characters long and contain at least one letter and one number' : ''}
-              />
-              <Button 
-                  variant="contained" 
-                  fullWidth 
-                  onClick={handleSignUp}
-                  disabled={isDisabled}
-                  sx={{ mt: 2 }}
-              >
-                  Sign Up
-              </Button>
-              <Button 
-                  variant="text" 
-                  fullWidth 
-                  sx={{ mt: 2 }}
-                  onClick={onSwitchPage}
-              >
-                  Already have an account? Login
-              </Button>
-          </Card>
-      </Container>
+        <Card sx={{ p: 4, mt: 8}}>
+          <Typography variant="h2" gutterBottom>Sign up to FitPal</Typography>
+          <TextField sx={{ mt: 8 }}
+              label="First Name" 
+              fullWidth 
+              margin="normal" 
+              value={firstName} 
+              onChange={(e) => setFirstName(e.target.value)}
+          />
+          <TextField 
+              label="Last Name" 
+              fullWidth 
+              margin="normal" 
+              value={lastName} 
+              onChange={(e) => setLastName(e.target.value)}
+          />
+          <TextField 
+              label="Email" 
+              fullWidth 
+              margin="normal" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
+              error={!!email && !emailRegex.test(email)}
+              helperText={!!email && !emailRegex.test(email) ? 'Invalid email format' : ''}
+          />
+          <TextField 
+              label="Password" 
+              type="password" 
+              fullWidth 
+              margin="normal" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+              error={!!password && !passwordSignUpRegex.test(password)}
+              helperText={
+                !!password && !passwordSignUpRegex.test(password) 
+                ? 'Password must be at least 8 characters long and contain at least one letter and one number' : ''
+              }
+          />
+          <Button 
+              variant="contained" 
+              fullWidth 
+              onClick={handleSignUp}
+              disabled={isDisabled}
+              sx={{ mt: 2 }}
+          >
+              Sign Up
+          </Button>
+          <Button 
+              variant="text" 
+              fullWidth 
+              sx={{ mt: 2 }}
+              onClick={onSwitchPage}
+          >
+              Already have an account? Login
+          </Button>
+      </Card>
+    </Container>
   );
 };
 
