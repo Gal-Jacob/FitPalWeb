94f5c80030d42cb1e94a785724ad454052e54c86
Ori Eluk <ori42410@gmail.com>
2025-03-18 20:50:21 +0200

    feat: added auth middleware to swagger and profile routes

---
+++ BackEnd/src/user/middleware.ts
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+interface AuthenticatedRequest extends Request {
+  user?: any;
+}
+
+const authMiddleware = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(' ')[1]; 
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied. No token provided.' });
+  }
+
+  try {
+    const secret = process.env.JWT_SECRET || "";
+    const decoded = jwt.verify(token, secret);
+    req.user = decoded; 
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token.' });
+  }
+};
+
+export default authMiddleware;
