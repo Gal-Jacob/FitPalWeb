47f95946a3a84b0037338d7700281858256f9389
Yoav David <yoavv.david2@gmail.com>
2025-03-19 14:57:56 +0200

    Formatting

--- BackEnd/src/utils/googlePassport.ts
+++ BackEnd/src/utils/googlePassport.ts
@@ -1,36 +1,42 @@
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import User from '../db/models/userModel';
+import passport from "passport";
+import dotenv from "dotenv";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../db/models/userModel";
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID || '',
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    callbackURL: '/api/user/google/callback'
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      let user = await User.findOne({ googleId: profile.id });
-      if (!user) {
-        user = new User({
-          googleId: profile.id,
-          email: profile.emails ? profile.emails[0].value : '',
-          firstName: profile.name?.givenName || '',
-          lastName: profile.name?.familyName || '',
-          accessToken,
-          refreshToken
-        });
-        await user.save();
-      } else {
-        user.accessToken = accessToken;
-        user.refreshToken = refreshToken;
-        await user.save();
+dotenv.config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      callbackURL: "/api/user/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            email: profile.emails ? profile.emails[0].value : "",
+            firstName: profile.name?.givenName || "",
+            lastName: profile.name?.familyName || "",
+            accessToken,
+            refreshToken,
+          });
+          await user.save();
+        } else {
+          user.accessToken = accessToken;
+          user.refreshToken = refreshToken;
+          await user.save();
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
       }
-      return done(null, user);
-    } catch (error) {
-      return done(error, false);
     }
-  }
-));
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user);
