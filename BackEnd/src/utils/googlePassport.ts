e1781721c7bed0d5663bb5fc334aaf9cdbaa9a4e
Ori Eluk <ori42410@gmail.com>
2025-03-19 21:33:27 +0200

    fix: made environment runnable

--- BackEnd/src/utils/googlePassport.ts
+++ BackEnd/src/utils/googlePassport.ts
@@ -10,25 +10,29 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-      callbackURL: '/api/user/google/callback'
+      callbackURL: '/api/user/google/callback',
     },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      let user = await User.findOne({ googleId: profile.id });
-      if (!user) {
-        user = new User({
-          googleId: profile.id,
-          email: profile.emails ? profile.emails[0].value : '',
-          firstName: profile.name?.givenName || '',
-          lastName: profile.name?.familyName || '',
-          accessToken,
-          refreshToken
-        });
-        await user.save();
-      } else {
-        user.accessToken = accessToken;
-        user.refreshToken = refreshToken;
-        await user.save();
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            email: profile.emails ? profile.emails[0].value : '',
+            firstName: profile.name?.givenName || '',
+            lastName: profile.name?.familyName || '',
+            accessToken,
+            refreshToken,
+          });
+          await user.save();
+        } else {
+          user.accessToken = accessToken;
+          user.refreshToken = refreshToken;
+          await user.save();
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
       }
     }
   )
