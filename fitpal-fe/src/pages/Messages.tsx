acbeb8d69994e14dc8699fc542de201152a40afd
ido sharonn <idosharon001@gmail.com>
2025-03-19 18:27:49 +0200

    last commit

--- fitpal-fe/src/pages/Messages.tsx
+++ fitpal-fe/src/pages/Messages.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Card, CardContent, Divider, Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography, Container, Button } from "@mui/material";
+import { Card, CardContent, Divider, Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography, Container, Button, Popper, Paper, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import { io, Socket } from "socket.io-client";
@@ -12,7 +12,15 @@ export type LastChatInfo = {
 
 const Messages: React.FC = () => {
   const [chats, setChats] = useState<LastChatInfo[]>([]);
+  const [showInput, setShowInput] = useState<boolean>(false);
+  const [addedUsername, setAddedUsername] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
   const navigate = useNavigate();
+
+  const handleAddClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+    setShowInput((prev) => !prev);
+  };
 
   const moveToChatRoom = (clickedChatData: LastChatInfo) => {
     navigate(`/chat/${clickedChatData.id}`, { state: { chat: clickedChatData } });  
@@ -21,6 +29,15 @@ const Messages: React.FC = () => {
   const addNewChat = (currentChatOtherUser: string) => {
     socketRef.current?.emit("createChat",currentChatOtherUser, localStorage.getItem("email"))
   }
+
+  const handleSubmit = () => {
+    if (addedUsername.trim()) {
+      console.log("New username:", addedUsername);
+      setAddedUsername("");
+      setShowInput(false);
+      setAnchorEl(null);
+    }
+  };
 
     const socketRef = useRef<Socket | null>(null)
 
@@ -46,9 +63,25 @@ const Messages: React.FC = () => {
             <Typography variant="h3" gutterBottom>
             Your chats
             </Typography>
-            <Button variant="contained" color="primary" onClick={() => addNewChat("ido")}>
+            <Button variant="contained" color="primary" onClick={handleAddClick}>
              +
             </Button>
+            <Popper open={showInput} anchorEl={anchorEl} placement="bottom-start">
+        <Paper style={{ padding: "10px", display: "flex", gap: "10px" }}>
+          <TextField
+            label="Enter Username"
+            variant="outlined"
+            value={addedUsername}
+            onChange={(e) => setAddedUsername(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
+        </Paper>
+      </Popper>
           </div>
           <Divider sx={{ marginBottom: 2, height: 4, borderBottomWidth: 4 }} />
           {chats.length > 0 ? (
@@ -80,7 +113,6 @@ const Messages: React.FC = () => {
           ) : (
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: "column", gap: '15px' }}>
               <Typography variant="h5">Oops, seems like you don't have any active chats :(</Typography>
-              <Button variant="contained">Create new chat</Button>
             </div>
           )}
         </CardContent>
